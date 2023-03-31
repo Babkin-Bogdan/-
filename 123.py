96075ee62c0803ca1c5fe56e8237edbe9e76cbc2
Babkin-Bogdan <bogdan4ikbabki@gmail.com>
2023-03-31 17:56:44 +0300

    калькулятор

---
+++ 123.py
@@ -0,0 +1,17 @@
+# x = int(input("first"))
+# b = int(input("second"))
+# print(x + b)
+a = float(input("первое число"))
+b = float(input("второе число"))
+c = input('какое мат действие нужно сделать?')
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    print(a / b)
+
+
+
